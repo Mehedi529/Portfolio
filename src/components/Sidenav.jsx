e5fb3b6efcd9fb5994cf11f13281e491bc2907b1
Mehedi529 <mehedihasan49265@gmail.com>
2023-11-14 20:08:08 +0600

    sidenav components updated

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,104 +1,67 @@
 import { useState } from "react";
-import {AiOutlineHome, AiOutlineMail, AiOutlineMenu, AiOutlineProfile} from 'react-icons/ai';
-import {BsPerson} from 'react-icons/bs';
-import {GrProjects} from 'react-icons/gr';
+import { AiOutlineHome, AiOutlineMail, AiOutlineMenu, AiOutlineProfile } from 'react-icons/ai';
+import { BsPerson } from 'react-icons/bs';
+import { GrProjects } from 'react-icons/gr';
+import './Sidenav.css'; // Import a separate CSS file for styling
 
 const Sidenav = () => {
-
-    const [nav,setNav] = useState(false);
+    const [nav, setNav] = useState(false);
+    const [hoveredItem, setHoveredItem] = useState(null);
 
     const handleNav = () => {
-      setNav(!nav);
-    }
+        setNav(!nav);
+        setHoveredItem(null); // Reset hoveredItem when toggling the navigation
+    };
+
+    const handleHover = (label) => {
+        setHoveredItem(label);
+    };
+
+    const navItems = [
+        { id: "main", icon: <AiOutlineHome size={20} />, label: "Home" },
+        { id: "work", icon: <GrProjects size={20} />, label: "Work" },
+        { id: "projects", icon: <AiOutlineProfile size={20} />, label: "Projects" },
+        { id: "resume", icon: <BsPerson size={20} />, label: "Resume" },
+        { id: "contact", icon: <AiOutlineMail size={20} />, label: "Contact" },
+    ];
 
     return (
         <div>
             <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden" />
-            {
-                nav ? (
-                    <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-                        <a onClick={handleNav} href="#main" 
-                           className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-                           >
-                            <AiOutlineHome size={20} />
-                            <span className="pl-4">Home</span>
+            {nav && (
+                <div className="nav-container">
+                    {navItems.map(({ id, icon, label }) => (
+                        <a
+                            key={id}
+                            href={`#${id}`}
+                            onClick={handleNav}
+                            onMouseEnter={() => handleHover(label)}
+                            onMouseLeave={() => handleHover(null)}
+                            className="nav-item"
+                        >
+                            {icon}
+                            {hoveredItem === label && <span className="label">{label}</span>}
                         </a>
-
-
-                        <a onClick={handleNav} href="#work" 
-                           className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-                           >
-                            <GrProjects size={20} />
-                            <span className="pl-4">Work</span>
-                        </a>
-
-
-                        <a onClick={handleNav} href="#projects" 
-                           className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-                           >
-                            <AiOutlineProfile size={20} />
-                            <span className="pl-4">Projects</span>
-                        </a>
-
-
-                        <a onClick={handleNav} href="#resume" 
-                           className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-                           >
-                            <BsPerson size={20} />
-                            <span className="pl-4">Resume</span>
-                        </a>
-
-
-                        <a onClick={handleNav} href="#contact" 
-                           className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-                           >
-                            <AiOutlineMail size={20} />
-                            <span className="pl-4">Contact</span>
-                        </a>
-
-
-                    </div>
-                )
-                : (
-                    ''
-                )
-            }
+                    ))}
+                </div>
+            )}
 
             <div className="md:block hidden fixed top-[25%] z-10">
                 <div className="flex flex-col">
-                   <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                      <AiOutlineHome size={20} />
-                    </a>
-
-
-                    <a href="#work" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                      <GrProjects size={20} />
-                    </a>
-
-
-                    <a href="#projects" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                      <AiOutlineProfile size={20} />
-                    </a>
-
-
-                    <a href="#resume" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                      <BsPerson size={20} />
-                    </a>
-
-
-                    <a href="#contact" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                      <AiOutlineMail size={20} />
-                    </a>
-
-
+                    {navItems.map(({ id, icon, label }) => (
+                        <a
+                            key={id}
+                            href={`#${id}`}
+                            onMouseEnter={() => handleHover(label)}
+                            onMouseLeave={() => handleHover(null)}
+                            className="nav-item"
+                        >
+                            {icon}
+                            {hoveredItem === label && <span className="label">{label}</span>}
+                        </a>
+                    ))}
                 </div>
             </div>
-
-            
-
-
-
-
         </div>
     );
 };
