0b182d5a177d9eba691687a1511536e312c2da14
Mehedi529 <mehedihasan49265@gmail.com>
2023-11-14 11:57:10 +0600

    resume updated and resume add

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,17 +1,24 @@
+/* eslint-disable no-unused-vars */
+// Resume.js
+
+import React from 'react';
 
 const Resume = () => {
+  // Replace 'your_resume_link' with the actual link you obtained
+  const resumeLink = 'https://drive.google.com/file/d/1Nw4n5XEOAsYByJ4-PHWikXNE7uU0X3V1/view?usp=sharing';
+
   const downloadResume = () => {
-    const link = document.createElement('a');
-    link.href = '/resume.pdf'; // Path to your resume file in the public folder
-    link.download = 'resume.pdf';
-    link.click();
+    window.open(resumeLink, '_blank');
   };
 
   return (
-    <div id="resume">
-      <h2>Your Portfolio</h2>
-      {/* Other portfolio content */}
-      <button onClick={downloadResume}>Download Resume</button>
+    <div id='resume' className="flex justify-center items-center mt-8">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={downloadResume}
+      >
+        Download Resume
+      </button>
     </div>
   );
 };
