4801dda54cf80110139d79f0c9d345dbabaf0862
Mehedi529 <mehedihasan49265@gmail.com>
2023-11-14 11:22:51 +0600

    contact form updated and Formspree apply

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,72 +1,125 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xrgwveyd");
 
-    const [formData, setFormData] = useState({
-        name: '',
-        number:'',
-        email: '',
-        subject: '',
-        message: ''
-
-    })
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({ ...prevData, [name]: value }));
-      };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Form submitted:' , formData);
-    };
-
-
-
-    return (
-        <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-
-            <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
-                Contact
-            </h1>
-
-            <form onSubmit={handleSubmit} action="" method="" encType="multipart/form-data">
-                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-
-                    <div className="flex flex-col">
-                        <label htmlFor="name" className="uppercase text-sm py-2">Name</label>
-                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <label htmlFor="number" className="uppercase text-sm py-2">Number</label>
-                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" id="number" name="number" value={formData.number} onChange={handleChange}/>
-                    </div>
-
-                    <div className="flex flex-col py-2">
-                        <label htmlFor="email" className="uppercase text-sm py-2">Email</label>
-                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" id="email" name="name" value={formData.email} onChange={handleChange} />
-                    </div>
-
-                    <div className="flex flex-col py-2">
-                        <label htmlFor="subject" className="uppercase text-sm py-2">Subject</label>
-                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" id="subject" name="subject" value={formData.subject} onChange={handleChange} />
-                    </div>
-
-                    <div className="flex flex-col py-4">
-                        <label htmlFor="message" className="uppercase text-sm py-2">Message</label>
-                        <textarea className="border-2 rounded-lg p-3 border-gray-300" rows='10' id="message" name="message" value={formData} onChange={handleChange}></textarea>
-
-                        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
-                        Send Message
-                        </button>
-                    </div>
-
-
-                </div>
-            </form>
+  if (state.succeeded) {
+    return(
+        <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 text-center">
+        <h1 className="py-4 text-4xl font-bold text-[#001b5e]">
+          Contact
+        </h1>
+        <div className="bg-green-200 text-green-800 p-4 rounded-md">
+          Thanks for joining!
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
+        Contact
+      </h1>
+
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto space-y-4">
+        <div>
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+          <ValidationError
+            prefix="Name"
+            field="name"
+            errors={state.errors}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email Address
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+            Phone Number
+          </label>
+          <input
+            id="phone"
+            type="tel"
+            name="phone"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+          <ValidationError
+            prefix="Phone"
+            field="phone"
+            errors={state.errors}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="subject" className="block text-sm font-medium text-gray-700">
+            Subject
+          </label>
+          <input
+            id="subject"
+            type="text"
+            name="subject"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+          <ValidationError
+            prefix="Subject"
+            field="subject"
+            errors={state.errors}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+
+        <button
+          type="submit"
+          disabled={state.submitting}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
